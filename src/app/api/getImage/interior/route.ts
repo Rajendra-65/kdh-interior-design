f0a8afc399ae9020b29ff182730e9c714295953c
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-05-31 14:58:17 +0530

    Fixing the API naming Convection

--- src/app/api/getImage/interior/route.ts
+++ src/app/api/getImage/interior/route.ts
@@ -5,6 +5,6 @@ import fs from "fs"
 export const GET = async () => {
     const imageDir = path.join(process.cwd(), 'public', 'Interior');
     const filenames = fs.readdirSync(imageDir);
-    const images = filenames.map((name) => `/interior/${name}`);
+    const images = filenames.map((name) => `/Interior/${name}`);
     return NextResponse.json({success:true,images:images})
 }
--- src/app/api/getImage/lightening/route.ts
+++ src/app/api/getImage/lightening/route.ts
@@ -5,6 +5,6 @@ import {NextResponse} from 'next/server';
 export const GET = async () => {
     const imageDir = path.join(process.cwd(), 'public', 'Lightening');
     const filenames = fs.readdirSync(imageDir);
-    const images = filenames.map((name) => `/lightening/${name}`);
+    const images = filenames.map((name) => `/Lightening/${name}`);
     return NextResponse.json({success:true,images:images})
 }
