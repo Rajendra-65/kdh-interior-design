82ff3d7cdb6bddc75ca74a5a112ccd60abbf5b48
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-06-02 13:27:34 +0530

    Added credential service,page,and route for signup and login

--- src/app/(signUp)/sign-up/page.tsx
+++ src/app/(signUp)/sign-up/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Button } from "@/components/ui/button"
 import {
     Card,
@@ -10,17 +11,53 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import Link from "next/link"
+import { useState } from "react"
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 
 const SignUp = () => {
+
+    const [firstName, setFirstName] = useState<string>('');
+    const [lastName, setLastName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    const HandleSignUp = () => {
+        if (firstName === '' || email === '' || password === '' || password.length < 8) {
+            alert("Please fill all the required fields");
+            <Alert>
+                <AlertTitle>Error!</AlertTitle>
+                <AlertDescription>
+                    Please fill all the required field in correct format
+                </AlertDescription>
+            </Alert>
+            return;
+        }
+
+        const signUpData = {
+            firstName,
+            lastName,
+            email,
+            password
+        }
+
+        console.log("Sign up Data:",signUpData);
+
+        // Reset the form fields after successful sign-up
+        setFirstName('');
+        setLastName('');
+        setEmail('');
+        setPassword('');
+    }
+
     return (
-        <div className = "flex items-center justify-center">
+        <div className="flex items-center justify-center">
             <Card className="w-[90vw] max-w-sm">
                 <CardHeader>
                     <CardTitle>Create an New account</CardTitle>
-                    <CardDescription className = "mt-2">
-                        Already have an account ?&nbsp; 
-                        <Link 
-                            className="text-sm text-cyan-400 underline underline-offset-1" 
+                    <CardDescription className="mt-2">
+                        Already have an account ?&nbsp;
+                        <Link
+                            className="text-sm text-cyan-400 underline underline-offset-1"
                             href="/log-in"
                         >
                             login to your account
@@ -37,6 +74,10 @@ const SignUp = () => {
                                     type="text"
                                     placeholder="john"
                                     required
+                                    value ={firstName}
+                                    onChange={(e) => {
+                                        setFirstName(e.target.value)
+                                    }}
                                 />
                             </div>
                             <div className="grid gap-2">
@@ -45,6 +86,12 @@ const SignUp = () => {
                                     id="lastName"
                                     type="text"
                                     placeholder="Doe"
+                                    value = {lastName}
+                                    onChange={
+                                        (e) => {
+                                            setLastName(e.target.value)
+                                        }
+                                    }
                                 />
                             </div>
                             <div className="grid gap-2">
@@ -53,7 +100,13 @@ const SignUp = () => {
                                     id="email"
                                     type="email"
                                     placeholder="m@example.com"
+                                    value={email}
                                     required
+                                    onChange={
+                                        (e) => {
+                                            setEmail(e.target.value)
+                                        }
+                                    }
                                 />
                             </div>
                             <div className="grid gap-2">
@@ -66,18 +119,29 @@ const SignUp = () => {
                                         Forgot your password?
                                     </Link>
                                 </div>
-                                <Input 
-                                    id="password" 
-                                    type="password" 
-                                    required 
-                                    placeholder = "At least 8 characters"
+                                <Input
+                                    id="password"
+                                    type="password"
+                                    required
+                                    placeholder="At least 8 characters"
+                                    value = {password}
+                                    onChange={
+                                        (e) => {
+                                            setPassword(e.target.value)
+                                        }
+                                    }
                                 />
                             </div>
                         </div>
                     </form>
                 </CardContent>
                 <CardFooter className="flex-col gap-2">
-                    <Button type="submit" className="w-full">
+                    <Button
+                        type="submit"
+                        className="w-full"
+                        onClick={() => {
+                            HandleSignUp()
+                        }}>
                         Signup
                     </Button>
                 </CardFooter>
